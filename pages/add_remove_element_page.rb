1924d96a01a55da9cd55a03c8f4cb36805b2e102
lourdesgonzalez <58890693+lourdesgonzalez@users.noreply.github.com>
2021-04-26 13:05:07 -0300

    Update add_remove_element_page.rb

--- pages/add_remove_element_page.rb
+++ pages/add_remove_element_page.rb
@@ -1,9 +1,9 @@
 class AddRemoveElement < Main
     def add_element_button
-        @browser.button(text: 'Add Element')
+        @browser.button(text: "Add Element")
     end
     
     def delete_button
-        @browser.button(text: 'Delete')
+        @browser.button(text: "Delete")
     end 
 end 
