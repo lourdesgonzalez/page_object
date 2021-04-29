445226c3127b4d9ed6c7c6c16d3a6670324c11f5
Lourdes <lourdeslaragonzalez27@gmail.com>
2021-04-29 15:45:32 -0300

    style_fixes_tests

--- tests/checkboxes_test.rb
+++ tests/checkboxes_test.rb
@@ -11,27 +11,27 @@ class CheckboxesTest
   include RSpec::Matchers
 
   def caso_1
-      driver = Driver.new 
-      context = Main.new(driver.new_browser)  # le paso este argumento porque hereda el initialize de basepage. 
-      context.go_to('https://the-internet.herokuapp.com/')
-      context.checkboxes_link.click 
-      context = Checkboxes.new(context.browser)
-      context.checkbox_1.click
-      expect(context.checkbox_1.attribute_list.include?(:checked)).to be(true)
-      context.browser.close
-      context.browser.quit
+    driver = Driver.new 
+    context = Main.new(driver.new_browser)  # le paso este argumento porque hereda el initialize de basepage. 
+    context.go_to('https://the-internet.herokuapp.com/')
+    context.checkboxes_link.click 
+    context = Checkboxes.new(context.browser)
+    context.checkbox_1.click
+    expect(context.checkbox_1.attribute_list.include?(:checked)).to be(true)
+    context.browser.close
+    context.browser.quit
   end
   
   def caso_2
-      driver = Driver.new 
-      context = Main.new(driver.new_browser)
-      context.go_to('https://the-internet.herokuapp.com/')
-      context.checkboxes_link.click 
-      context = Checkboxes.new(context.browser)
-      context.checkbox_2.click
-      expect(!(context.checkbox_2.attribute_list.include?(:checked))).to be(true)
-      context.browser.close
-      context.browser.quit
+    driver = Driver.new 
+    context = Main.new(driver.new_browser)
+    context.go_to('https://the-internet.herokuapp.com/')
+    context.checkboxes_link.click 
+    context = Checkboxes.new(context.browser)
+    context.checkbox_2.click
+    expect(!(context.checkbox_2.attribute_list.include?(:checked))).to be(true)
+    context.browser.close
+    context.browser.quit
   end
 end
 
--- tests/hovers_test.rb
+++ tests/hovers_test.rb
@@ -1,70 +1,70 @@
 require 'watir'
 require 'pry'
 require 'rspec'
-require "rspec/expectations"
-require "../support/driver.rb"
-require "../pages/base.rb"
-require "../pages/main.rb"
-require "../pages/hovers_page.rb"
+require 'rspec/expectations'
+require '../support/driver.rb'
+require '../pages/base.rb'
+require '../pages/main.rb'
+require '../pages/hovers_page.rb'
 
 class HoversTest
-    include RSpec::Matchers
+  include RSpec::Matchers
 
-    def caso_1
-        driver = Driver.new 
-        context = Main.new(driver.new_browser)   
-        context.go_to('https://the-internet.herokuapp.com/')   
-        context.hovers_link.click
-        context = Hovers.new(context.browser)
-        context.first_image.hover
-        expect(context.first_user.present?).to be(true)
-        expect(context.view_profile_link_us1.present?).to be(true)
-        context.second_image.hover
-        expect(context.second_user.present?).to be(true)
-        expect(context.view_profile_link_us2.present?).to be(true)
-        context.third_image.hover
-        expect(context.third_user.present?).to be(true)
-        expect(context.view_profile_link_us3.present?).to be(true)
-    end 
+  def caso_1
+    driver = Driver.new 
+    context = Main.new(driver.new_browser)   
+    context.go_to('https://the-internet.herokuapp.com/')   
+    context.hovers_link.click
+    context = Hovers.new(context.browser)
+    context.first_image.hover
+    expect(context.first_user.present?).to be(true)
+    expect(context.view_profile_link_us1.present?).to be(true)
+    context.second_image.hover
+    expect(context.second_user.present?).to be(true)
+    expect(context.view_profile_link_us2.present?).to be(true)
+    context.third_image.hover
+    expect(context.third_user.present?).to be(true)
+    expect(context.view_profile_link_us3.present?).to be(true)
+  end 
 
-    def caso_2
-        driver = Driver.new 
-        context = Main.new(driver.new_browser)   
-        context.go_to('https://the-internet.herokuapp.com/')   
-        context.hovers_link.click
-        context = Hovers.new(context.browser)
-        context.first_image.hover
-        expect(context.first_user.present?).to be(true)
-        expect(context.view_profile_link_us1.present?).to be(true)
-        context.view_profile_link_us1.click
-        expect(context.error.present?).to be(true)
-    end
+  def caso_2
+    driver = Driver.new 
+    context = Main.new(driver.new_browser)   
+    context.go_to('https://the-internet.herokuapp.com/')   
+    context.hovers_link.click
+    context = Hovers.new(context.browser)
+    context.first_image.hover
+    expect(context.first_user.present?).to be(true)
+    expect(context.view_profile_link_us1.present?).to be(true)
+    context.view_profile_link_us1.click
+    expect(context.error.present?).to be(true)
+  end
 
-    def caso_3
-        driver = Driver.new 
-        context = Main.new(driver.new_browser)   
-        context.go_to('https://the-internet.herokuapp.com/')   
-        context.hovers_link.click
-        context = Hovers.new(context.browser)
-        context.second_image.hover
-        expect(context.second_user.present?).to be(true)
-        expect(context.view_profile_link_us2.present?).to be(true)
-        context.view_profile_link_us2.click
-        expect(context.error.present?).to be(true)
-    end 
+  def caso_3
+    driver = Driver.new 
+    context = Main.new(driver.new_browser)   
+    context.go_to('https://the-internet.herokuapp.com/')   
+    context.hovers_link.click
+    context = Hovers.new(context.browser)
+    context.second_image.hover
+    expect(context.second_user.present?).to be(true)
+    expect(context.view_profile_link_us2.present?).to be(true)
+    context.view_profile_link_us2.click
+    expect(context.error.present?).to be(true)
+  end 
 
-    def caso_4
-        driver = Driver.new 
-        context = Main.new(driver.new_browser)   
-        context.go_to('https://the-internet.herokuapp.com/')   
-        context.hovers_link.click
-        context = Hovers.new(context.browser)
-        context.third_image.hover
-        expect(context.third_user.present?).to be(true)
-        expect(context.view_profile_link_us3.present?).to be(true)
-        context.view_profile_link_us3.click
-        expect(context.error.present?).to be(true)
-    end
+  def caso_4
+    driver = Driver.new 
+    context = Main.new(driver.new_browser)   
+    context.go_to('https://the-internet.herokuapp.com/')   
+    context.hovers_link.click
+    context = Hovers.new(context.browser)
+    context.third_image.hover
+    expect(context.third_user.present?).to be(true)
+    expect(context.view_profile_link_us3.present?).to be(true)
+    context.view_profile_link_us3.click
+    expect(context.error.present?).to be(true)
+  end
 end 
 
 test_hovers = HoversTest.new
